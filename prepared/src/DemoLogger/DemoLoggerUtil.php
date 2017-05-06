11a527dd6935056b8d816137c98d6d5e51fa1b7b
Edward Barnard <ewbarnard@embarqmail.com>
2017-05-06 11:31:01 -0500

    Mockery is working

--- prepared/src/DemoLogger/DemoLoggerUtil.php
+++ prepared/src/DemoLogger/DemoLoggerUtil.php
@@ -8,20 +8,67 @@
 
 namespace App\DemoLogger;
 
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
 final class DemoLoggerUtil {
     /** @var DemoLoggerUtil */
     private static $instance;
 
+    /** @var AMQPStreamConnection */
+    private $connection;
+
+    /** @var AMQPChannel */
+    private $channel;
+
     /**
      * DemoLoggerUtil constructor: All public methods are static
+     * Allows dependency injection for unit testing
+     *
+     * @param array $dependencies
      */
-    private function __construct() {
+    private function __construct(array $dependencies = null) {
+        if (is_array($dependencies)) {
+            $this->injectDependencies($dependencies);
+        }
     }
 
-    public static function getInstance() {
+    /**
+     * Inject dependencies during unit testing
+     *
+     * @param array $dependencies
+     */
+    private function injectDependencies(array $dependencies) {
+        foreach ($dependencies as $key => $value) {
+            if (property_exists(static::class, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+
+    public static function getInstance(array $dependencies = null) {
         if (!static::$instance) {
-            static::$instance = new static;
+            static::$instance = new static($dependencies);
         }
         return static::$instance;
+    }
+
+    public static function log($event, $detail = '') {
+    }
+
+    public static function finalize() {
+        static::getInstance()->disconnectRabbitMQ();
+    }
+
+    private function disconnectRabbitMQ() {
+        $this->channel->close();
+        $this->connection->close();
+    }
+
+    /**
+     * Unit test support, called during tearDown()
+     */
+    public static function reset() {
+        static::$instance = null;
     }
 }
--- prepared/tests/TestCase/DemoLogger/DemoLoggerTest.php
+++ prepared/tests/TestCase/DemoLogger/DemoLoggerTest.php
@@ -9,11 +9,46 @@
 namespace App\DemoLogger;
 
 use Cake\TestSuite\IntegrationTestCase;
+use \Mockery as m;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class DemoLoggerTest extends IntegrationTestCase {
+
+    public function tearDown() {
+        DemoLoggerUtil::reset();
+        m::close();
+    }
 
     public function testGetInstance() {
         $instance = DemoLoggerUtil::getInstance();
         static::assertInstanceOf(DemoLoggerUtil::class, $instance);
+    }
+
+    public function testGetInstanceDependencies() {
+        $dependencies = ['connection' => null];
+        $instance = DemoLoggerUtil::getInstance($dependencies);
+        static::assertInstanceOf(DemoLoggerUtil::class, $instance);
+    }
+
+    public function testFinalizeClosesChannel() {
+        $this->setFinalizeDependencies();
+        DemoLoggerUtil::finalize();
+    }
+
+    public function testFinalizeClosesConnection() {
+        $this->setFinalizeDependencies();
+        DemoLoggerUtil::finalize();
+    }
+
+    private function setFinalizeDependencies() {
+        /** @var m\Mock $channel */
+        $channel = m::mock(AMQPChannel::class);
+        $channel->shouldReceive('close')->once();
+        /** @var m\Mock $connection */
+        $connection = m::mock(AMQPStreamConnection::class);
+        $connection->shouldReceive('close')->once();
+        $dependencies = ['channel' => $channel, 'connection' => $connection];
+        DemoLoggerUtil::getInstance($dependencies);
     }
 }
