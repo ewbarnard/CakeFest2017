ca09f83894a6fce94d9475d2159aa764659c9e45
Edward Barnard <ewbarnard@embarqmail.com>
2017-05-06 14:59:35 -0500

    Multi-table consumer complete

--- prepared/src/Shell/ConsumerShell.php
+++ prepared/src/Shell/ConsumerShell.php
@@ -8,6 +8,7 @@
 
 namespace App\Shell;
 
+use App\DemoLogger\LookupUtil;
 use Cake\Console\Shell;
 use Cake\Database\Connection;
 use Cake\Database\StatementInterface;
@@ -16,6 +17,8 @@ use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 class ConsumerShell extends Shell {
+    private static $cacheLimit = 200;
+
     private static $exchange = 'fest';
 
     private static $routingKey = 'fest_log';
@@ -29,7 +32,20 @@ class ConsumerShell extends Shell {
     private $channel;
 
     /** @var StatementInterface */
-    private $insertDemoLog;
+    private $queryInstance;
+
+    /** @var StatementInterface */
+    private $insertInstance;
+
+    /** @var StatementInterface */
+    private $insertLog;
+
+    /** @var Connection */
+    private $db;
+
+    private $message;
+
+    private $instanceCache = [];
 
     public function main() {
         $this->myInitialize();
@@ -51,15 +67,22 @@ class ConsumerShell extends Shell {
     }
 
     private function prepareStatements() {
-        /** @var Connection $connection */
-        $connection = ConnectionManager::get('fest');
+        $this->db = ConnectionManager::get('fest');
 
-        $sql = 'INSERT INTO fest_log_v01 
-        (archive_month, instance_code, hostname, instance_begin, event_time, event_class, event_function, event, 
-        detail, created) 
+        $sql = 'SELECT id FROM fest_instances WHERE archive_month = ? AND instance_code = ? LIMIT 1';
+        $this->queryInstance = $this->db->prepare($sql);
+
+        $sql = 'INSERT INTO fest_instances 
+        (archive_month, fest_server_id, instance_code, instance_begin, created) 
         VALUES 
-        (month(curdate()), ?, ?, ?, ?, ?, ?, ?, ?, now())';
-        $this->insertDemoLog = $connection->prepare($sql);
+        (month(curdate()), ?, ?, ?, now())';
+        $this->insertInstance = $this->db->prepare($sql);
+
+        $sql = 'INSERT INTO fest_logs 
+        (archive_month, fest_instance_id, fest_class_id, fest_function_id, fest_event_id, event_time, detail, created) 
+        VALUES 
+        (month(curdate()), ?, ?, ?, ?, ?, ?, now())';
+        $this->insertLog = $this->db->prepare($sql);
     }
 
     private function consume() {
@@ -75,28 +98,55 @@ class ConsumerShell extends Shell {
     }
 
     public function processMessage($payload) {
-        $message = json_decode($payload, true);
+        $this->message = json_decode($payload, true);
         $this->verbose($payload);
-        if (is_array($message) && count($message)) {
+        if (is_array($this->message) && count($this->message)) {
+            $detail = $this->message['event']['detail'];
+            $detail = substr($detail, 0, 255);
             $parms = [
-                $message['meta']['instanceCode'],
-                $this->varchar($message['meta']['server']),
-                $this->timestamp($message['meta']['begin']),
-                $this->timestamp($message['event']['begin']),
-                $this->varchar($message['event']['class']),
-                $this->varchar($message['event']['function']),
-                $this->varchar($message['event']['event']),
-                $this->varchar($message['event']['detail']),
+                $this->lookupInstance(),
+                $this->lookup('fest_classes', 'class'),
+                $this->lookup('fest_functions', 'function'),
+                $this->lookup('fest_events', 'event'),
+                $this->timestamp('event'),
+                $detail,
             ];
-            $this->insertDemoLog->execute($parms);
+            $this->insertLog->execute($parms);
         }
     }
 
-    private function varchar($string, $length = 255) {
-        return substr($string, 0, $length);
+    private function lookupInstance() {
+        $code = $this->message['meta']['instanceCode'];
+        if (array_key_exists($code, $this->instanceCache)) {
+            return $this->instanceCache[$code];
+        }
+        if (count($this->instanceCache) >= static::$cacheLimit) {
+            $this->instanceCache = [];
+        }
+        $parms = [date('n'), $code];
+        $this->queryInstance->execute($parms);
+        $row = $this->queryInstance->fetch('assoc');
+        if (is_array($row) && count($row)) {
+            $id = (int)$row['id'];
+        } else {
+            $server = $this->message['meta']['server'];
+            $serverId = LookupUtil::lookup($this->db, 'fest_servers', $server);
+            $begin = $this->timestamp('meta');
+            $parms = [$serverId, $code, $begin];
+            $this->insertInstance->execute($parms);
+            $id = (int)$this->insertInstance->lastInsertId();
+        }
+        $this->instanceCache[$code] = $id;
+        return $id;
     }
 
-    private function timestamp($microtime) {
+    private function timestamp($section) {
+        $microtime = $this->message[$section]['begin'];
         return date('Y-m-d H:i:s', (int)$microtime);
+    }
+
+    private function lookup($table, $field) {
+        $value = $this->message['event'][$field];
+        return LookupUtil::lookup($this->db, $table, $value);
     }
 }
