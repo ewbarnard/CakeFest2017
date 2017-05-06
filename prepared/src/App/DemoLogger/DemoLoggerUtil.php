f70a0dc664d8d906646c2e2e6ff185bab8662a17
Edward Barnard <ewbarnard@embarqmail.com>
2017-05-06 10:53:21 -0500

    Placeholder new class

---
+++ prepared/src/App/DemoLogger/DemoLoggerUtil.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ewb
+ * Date: 5/6/17
+ * Time: 10:48 AM
+ */
+
+namespace App\DemoLogger;
+
+final class DemoLoggerUtil {
+
+    /**
+     * DemoLoggerUtil constructor: All public methods are static
+     */
+    private function __construct() {
+    }
+
+    public static function getInstance() {
+        return null;
+    }
+}
